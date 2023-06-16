9daa7daebe20770a6229d2770d14cd2aeccf93ef
someone2639 <>
2023-06-15 21:30:09 -0400

    files

---
+++ src/emojify.rs
@@ -0,0 +1,77 @@
+use image::{
+    GenericImageView,
+    DynamicImage,
+    ImageBuffer,
+    Rgba,
+    imageops,
+    imageops::FilterType::Lanczos3,
+};
+use image::io::Reader as ImageReader;
+use std::collections::HashMap;
+use super::rgb2emoji;
+use itertools::izip;
+
+fn pack_color(payload: (u32, u32, &Rgba<u8>)) -> (u32, u32, u32) {
+    let px = payload.2;
+    let color = ((px[0] as u32) << 24)
+         | ((px[1] as u32) << 16)
+         | ((px[2] as u32) << 8)
+         | (px[3] as u32);
+
+    return (payload.0, payload.1, color);
+}
+
+pub fn emojify(tbl: &HashMap<u32, char>, width: u32, path: &String) {
+
+    let binding = image::open(path).unwrap()
+        .resize(width, 999999, Lanczos3);
+    let img = binding.as_rgba8().unwrap();
+
+    let (mut wd, ht) = img.dimensions();
+
+    let mut emojis : Vec<char> = Vec::new();
+    
+    let mut resultImg = DynamicImage::new_rgba8(wd * 16, ht * 16);
+    // let colors = pixels;
+
+    let pixels = img.enumerate_pixels().map(pack_color);
+
+    let mut emojis : Vec<char> = Vec::new();
+    for i in pixels {
+        emojis.push(rgb2emoji::search(tbl, i.2));
+    }
+
+
+
+
+
+
+    // emojis.iter().map(commit_pixel);
+
+    let mut x = 0;
+    let mut y = 0;
+
+    let mut cursor = 0;
+
+    wd -= 1;
+
+    for i in emojis {
+        let home = std::env::var("HOME").unwrap();
+
+        let filepath = format!("{}/Devel/twemoji/assets/16x16/{:x}.png", home, i as u32);
+        let em = image::open(filepath).unwrap();
+
+        imageops::overlay(&mut resultImg, &em, x, y);
+
+        cursor += 1;
+        x += 16;
+        if cursor > wd {
+            x = 0;
+            cursor = 0;
+            y += 16;
+        }
+    }
+
+    resultImg.save("output.png");
+}
+
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#![feature(const_trait_impl)]
+#[macro_use] extern crate maplit;
+
+use std::process::exit;
+mod emojify;
+mod rgb2emoji;
+
+
+fn main() {
+    let args : Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: {} image_width path/to/image", std::env::args().nth(0).unwrap());
+        exit(1);
+    }
+            let im_width : u32 = str::parse::<u32>(&args[1]).unwrap();
+            let filepath : &String = &args[2];
+            let emojiTable = rgb2emoji::generate();
+            emojify::emojify(&emojiTable, im_width, filepath);
+
+    // match args.len() {
+    //     3 => {
+    //     },
+    //     4 => {
+    //         let im_width : u32 = str::parse::<u32>(&args[2]).unwrap();
+    //         let filepath : &String = &args[3];
+    //         let emoji_map = rgb2emoji::generate();
+    //         emojify::emojify(&emoji_map, im_width, filepath);
+    //     },
+    //     _ => {
+    //         println!("Usage: {} image_width path/to/image", std::env::args().nth(0).unwrap());
+    //         exit(1);
+    //     }
+    // }
+}
